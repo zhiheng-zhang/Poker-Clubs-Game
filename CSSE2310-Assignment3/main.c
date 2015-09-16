bbe4340e053a0db9ff28fabe6c64f0eadf567b4f
ZHANGZHIHENG <LukeZhang@ZHANGZHIHENGdeMacBook-Pro.local>
2015-09-16 15:08:15 +1000

    Initial Commit

---
+++ CSSE2310-Assignment3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSSE2310-Assignment3
+//
+//  Created by ZHANGZHIHENG on 15/9/16.
+//  Copyright (c) 2015年 ZHANGZHIHENG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
